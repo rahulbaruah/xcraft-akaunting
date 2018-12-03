20ee7e7a49bc8cb04db77c4ad05c01e41ed57fc9
rahulbaruah <baruah.rahul.88@gmail.com>
2018-12-03 16:56:14 +0000

    initial commit

---
+++ config/version.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+
+    'name'      =>  'Akaunting',
+
+    'code'      =>  'Reconciliation',
+
+    'major'     =>  '1',
+
+    'minor'     =>  '3',
+
+    'patch'     =>  '4',
+
+    'build'     =>  '',
+
+    'status'    =>  'Stable',
+
+    'date'      =>  '30-November-2018',
+
+    'time'      =>  '18:30',
+
+    'zone'      =>  'GMT +3',
+
+];
